8a9d7f85617203b847c44020d691b05481617b16
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-02-14 18:05:46 -0500

    Implement date picker

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,19 @@
-async function getData() {
+const datePicker = document.querySelector("#date-picker");
+const imgContainer = document.querySelector("#img-container");
+let apiUrl = "https://api.nasa.gov/planetary/apod?api_key=5XYp2eBUxpJyA0uaDnNiXIB2JKmBcTIz63xt2j6B";
+
+async function getData(date) {
   try {
+    // if date entered, update url
+    if (date) {
+      apiUrl = `https://api.nasa.gov/planetary/apod?api_key=5XYp2eBUxpJyA0uaDnNiXIB2JKmBcTIz63xt2j6B&date=${date}`
+    }
     // get data from API call
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=5XYp2eBUxpJyA0uaDnNiXIB2JKmBcTIz63xt2j6B`)
+    const response = await fetch(apiUrl)
     const data = await response.json();
     console.log(data);
 
     // add content to page
-    const imgContainer = document.querySelector("#img-container");
     imgContainer.innerHTML = `
       <img src="${data.url}" alt="${data.title}" />
       <p class="caption">${data.explanation}</p>
@@ -17,3 +24,8 @@ async function getData() {
   }
 }
 getData();
+
+datePicker.addEventListener('change', () => {
+  date = datePicker.value;
+  getData(date);
+})
