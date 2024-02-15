19657425b77b3e07a58693bb1c2f0c309695e78a
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-02-14 19:24:44 -0500

    Account for UTC offset when getting today's date

--- public/js/main.js
+++ public/js/main.js
@@ -2,16 +2,18 @@ const datePicker = document.querySelector("#date-picker");
 const imgContainer = document.querySelector("#img-container");
 let apiUrl = "https://api.nasa.gov/planetary/apod?api_key=5XYp2eBUxpJyA0uaDnNiXIB2JKmBcTIz63xt2j6B";
 
+// get today's date as YYYY-MM-DD, accounting for UTC offset
+let today = new Date();
+const offset = today.getTimezoneOffset();
+today = new Date(today.getTime() - (offset*60*1000));
+today = today.toISOString().substring(0,10);
+let date = today;
 // prevent picking future dates
-const today = new Date().toISOString().substring(0,10); // today's date as YYYY-MM-DD
 datePicker.setAttribute("max", today);
 
 async function getData(date) {
   try {
-    // if date picked, update url
-    if (date) {
-      apiUrl = `https://api.nasa.gov/planetary/apod?api_key=5XYp2eBUxpJyA0uaDnNiXIB2JKmBcTIz63xt2j6B&date=${date}`
-    }
+    apiUrl = `https://api.nasa.gov/planetary/apod?api_key=5XYp2eBUxpJyA0uaDnNiXIB2JKmBcTIz63xt2j6B&date=${date}`
     // get data from API call
     const response = await fetch(apiUrl)
     const data = await response.json();
@@ -27,7 +29,7 @@ async function getData(date) {
     console.warn(error);
   }
 }
-getData();
+getData(date);
 
 // on date being picked, update date & get new content
 datePicker.addEventListener('change', () => {
